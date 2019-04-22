ccd429c75291993b19f66d267978484fab7e4399
John Stevenson <john@jr0cket.co.uk>
2019-04-22 16:57:13 +0100

    Helper function: Generating unordered lists
    Iterate over a collection of strings and create an unordered list.

--- src/clojurebridge_london_landing_page/content_ideas.cljs
+++ src/clojurebridge_london_landing_page/content_ideas.cljs
@@ -5,6 +5,27 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
+;; Helper function: Generating content structure
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; write functions that generate boilerplate code such as unordered lists
+;; Pass in a collection of list items
+
+(defn unordered-list
+  "Creates an unordered list from a collection of list item names.
+
+  Each item in the list should be a string"
+  [list-items]
+  (into [:ul]
+        (for [item list-items]
+          [:li item])))
+
+(unordered-list ["one" "two" "three"])
+
+
+;; Using Tabs in the footer of a hero section
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 ;; Footer with tabs to select specific content
 ;; An alternative to the above Navigation bar
 
