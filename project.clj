a018572394651ba2bed4c51a38088c5e644a59a4
John Stevenson <john@jr0cket.co.uk>
2019-06-13 18:18:11 +0100

    Live profile for deploy build
    Added a live alias to figwheel-main to create the build file the same as the dev
    profile, except the resulting javascript file is save in `/docs/cljs-out/`

--- project.clj
+++ project.clj
@@ -15,6 +15,7 @@
   :aliases {"fig"       ["trampoline" "run" "-m" "figwheel.main"]
             "fig:build" ["trampoline" "run" "-m" "figwheel.main" "-b" "dev" "-r"]
             "fig:min"   ["run" "-m" "figwheel.main" "-O" "advanced" "-bo" "dev"]
+            "fig:live"  ["run" "-m" "figwheel.main" "-O" "advanced" "-bo" "live"]
             "fig:test"  ["run" "-m" "figwheel.main" "-co" "test.cljs.edn" "-m" clojurebridge-london-landing-page.test-runner]}
 
   :profiles {:dev {:dependencies [[com.bhauman/figwheel-main "0.1.9"]
